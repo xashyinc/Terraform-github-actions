e7f45ad3e19e63e91b1065e3ffc0bfdf0388ca4b
Cedricade1 <108162472+Cedricade1@users.noreply.github.com>
2023-02-20 01:52:58 -0500

    jjjj

--- main.tf
+++ main.tf
@@ -52,6 +52,6 @@ terraform {
     bucket = "cedric-test-ade"
     key    = "terraform.tfstate"
     region = "us-west-1"
-    access_key =  ${{secret_key}}
-    secret_key =  ${{secret_key}}
+    access_key =  ${{ secret_key }}
+    secret_key =  ${{ secret_access_key }}
 }
