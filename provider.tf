a07af64d29578b6665abdfc073c4036d5781fe42
Cedricade1 <108162472+Cedricade1@users.noreply.github.com>
2023-02-20 21:11:57 -0500

    variable

--- provider.tf
+++ provider.tf
@@ -8,7 +8,8 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.region
-  access_key = "AKIA2UWAKQ47WJW4Q5M7"
-  secret_key = "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"
+  region = var.region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
+
