37f6f6c6026bdc45a36b4bafe42af162e57fe16b
Cedricade1 <108162472+Cedricade1@users.noreply.github.com>
2023-02-22 11:26:47 -0500

    no comments

--- provider.tf
+++ provider.tf
@@ -9,15 +9,11 @@ terraform {
     bucket = "cedric-test-ade"
     key    = "terraform.tfstate"
     region = "us-west-1"
-    #access_key = "AKIA2UWAKQ47WJW4Q5M7"
-    #secret_key =  "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"
 
   }
 }
 
 provider "aws" {
   region = var.region
-  #access_key = "AKIA2UWAKQ47WJW4Q5M7"
- # secret_key = "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"
 }
 
