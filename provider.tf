40577458177703f82411becca276a338df0231dc
Cedricade1 <108162472+Cedricade1@users.noreply.github.com>
2023-02-22 03:29:21 -0500

    new backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~>4.0"
     }
   }
+  backend "s3" {
+    bucket = "cedric-test-ade"
+    key    = "terraform.tfstate"
+    region = "us-west-1"
+    #access_key = "AKIA2UWAKQ47WJW4Q5M7"
+    #secret_key =  "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"
+
+  }
 }
 
 provider "aws" {
